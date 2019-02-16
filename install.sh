a7f3f4a501a2f4a1639b40ffc8039fa862b1fefe
Ivan Korniux <main@korniux.net>
2019-02-16 09:53:46 +0500

    Tweaks for Darwin platform

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PLATFORM="$(uname)"
+CURRENT_FOLDER="$PWD"
 
 cat << EOF
 ##  ##
@@ -12,35 +13,41 @@ cat << EOF
 EOF
 
 function zshInstall() {
-    echo "Installing zsh"
-    case "$PLATFORM" in
-        Darwin)
-            brew install zsh
-            ;;
-        Linux)
-            sudo apt-get -y install zsh
-            ;;
-    esac
-    echo "Installing oh-my-zsh plugin"
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    echo "Installing syntax highlighting"
+    if [ `which zsh` == "" ];
+    then
+        echo "#### Installing zsh"
+        case "$PLATFORM" in
+            Darwin)
+               brew install zsh
+               ;;
+            Linux)
+               sudo apt-get -y install zsh
+               ;;
+        esac
+    fi
+    echo "#### Copying configuration"
+    yes | cp -f $PWD/.zshrc $HOME/
+    if [ ! -d $HOME/.oh-my-zsh  ];
+    then
+        echo "#### Installing oh-my-zsh plugin"
+        sh -c "$(curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+    echo "#### Installing syntax highlighting"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    echo "Copying configuration"
-    yes | cp -rf $PWD/.zshrc ~/
-    echo "$(which zsh) SUCCESS"
+    echo "#### $(which zsh) SUCCESS"
 }
 
 function vimInstall() {
-    echo "Installing vim\'s pathogen"
-    mkdir -p ~/.vim/autoload ~/.vim/bundle
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-    echo "Installing plugins"
-    cd ~/.vim/bundle
+    echo "#### Installing pathogen"
+    mkdir -p $HOME/.vim/autoload ~/.vim/bundle
+	curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    echo "#### Copying configuration"
+    yes | cp -f $PWD/.vimrc $HOME/
+    echo "#### Installing plugins"
+    cd $HOME/.vim/bundle
     git clone https://github.com/itchyny/lightline.vim.git
     git clone https://github.com/itchyny/vim-gitbranch.git
-    echo "Copying configuration"
-    yes | cp -rf .vimrc ~/
-    echo "$(which vim) SUCCESS"
+    echo "#### $(which vim) SUCCESS"
 }
 
 function usage() {
