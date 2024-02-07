c6349329f47106b21ef0633461f1f4d0638e35c3
Ian Korniux <main@korniux.net>
2024-02-07 22:01:28 +0500

    (chore) Fix OMP

--- zsh/.zshrc.d/40-oh-my-posh.sh
+++ zsh/.zshrc.d/40-oh-my-posh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-if [[ -z "$(which oh-my-posh)" ]];
+if [[ "$(which oh-my-posh)" ]];
 then
-  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
-else 
   eval "$(oh-my-posh init zsh --config $HOME/.omp/theme.omp.json)"
+else 
+  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 fi
